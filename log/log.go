69e31f8be8f54751596d717ddf67b5d72dce55d0
moodbase <moodbase@outlook.com>
2024-10-24 16:59:17 +0800

    update logger

--- log/log.go
+++ log/log.go
@@ -1,13 +1,7 @@
 package log
 
-import (
-	"log/slog"
-)
-
 // Logger is an ETH compatible log interface
 type Logger interface {
-	Log(level slog.Level, msg string, ctx ...interface{})
-	Trace(msg string, ctx ...interface{})
 	Debug(msg string, ctx ...interface{})
 	Info(msg string, ctx ...interface{})
 	Warn(msg string, ctx ...interface{})
